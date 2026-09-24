6edbab7fcc7da850c12cff57b1dc34a440cedcc9
ishak-abbaz <ishak.abbaz@univ-constantine2.dz>
2026-09-24 20:47:44 +0100

    changes

--- server/config/database.js
+++ server/config/database.js
@@ -3,13 +3,21 @@ const { Pool } = require('pg');
 // Ensure config file exists with environment variables
 require('dotenv').config();
 // New pool instance (instantiating connection to the database)
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+    : new Pool({
+        host: process.env.DB_HOST || 'postgres',
+        port: process.env.DB_PORT || 5432,
+        user: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME || 'webexcel'
+    });
+
 
 // Error handling
 pool.on('error', (err) => {
